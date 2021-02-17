4563544f345b3b07f34d9e5ec766ee4ad569cd7d
MnqobiSB <amandlamm1@gmail.com>
2021-02-17 09:25:54 +0200

    setup front-end routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+// Front-End Routes
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function (req, res, next) {
+// 	res.render('index', { title: 'Express' });
+// });
+
+// GET / landing page
+router.get('/', (req, res) => {
+	res.send('Welocome to the Home Page');
+});
+// GET /about
+router.get('/about', (req, res) => {
+	res.send('Welocome to the About Us Page');
+});
+// Get /staff
+router.get('/staff', (req, res) => {
+	res.send('Welocome to the Staff Page');
+});
+// GET /gallery
+router.get('/gallery', (req, res) => {
+	res.send('Welocome to the Gallery Page');
+});
+// GET /calender
+router.get('/calenda', (req, res) => {
+	res.send('Welocome to the Calenda Page');
+});
+// GET /documents
+router.get('/documents', (req, res) => {
+	res.send('Welocome to the Documents Page');
 });
 
 module.exports = router;
