bb826ca9e04b64e92cc8e824c9eebd5e8cdc1e0e
MnqobiSB <amandlamm1@gmail.com>
2021-02-19 18:05:58 +0200

    setup the boilerplate, code the navbar, add the covid & values  section

--- routes/index.js
+++ routes/index.js
@@ -11,31 +11,43 @@ router.get('/', asyncErrorHandler(landingPage));
 // GET /about
 router.get('/about-us', (req, res) => {
 	res.render('about', {
-		title: 'About Us'
+		title: 'About Us',
+		page: 'about'
 	});
 });
 // Get /staff
 router.get('/staff', (req, res) => {
 	res.render('staff', {
-		title: 'Meet The Staff'
+		title: 'Meet The Staff',
+		page: 'staff'
 	});
 });
 // GET /gallery
 router.get('/gallery', (req, res) => {
 	res.render('gallery', {
-		title: 'Our Gallery'
+		title: 'Our Gallery',
+		page: 'gallery'
 	});
 });
 // GET /calender
 router.get('/calenda', (req, res) => {
 	res.render('calenda', {
-		title: 'School Calenda'
+		title: 'School Calenda',
+		page: 'calenda'
 	});
 });
 // GET /documents
 router.get('/documents', (req, res) => {
 	res.render('documents', {
-		title: 'Available Documents'
+		title: 'Available Documents',
+		page: 'documents'
+	});
+});
+// GET /contact
+router.get('/contact', (req, res) => {
+	res.render('contact', {
+		title: 'Contact Us',
+		page: 'contact'
 	});
 });
 
