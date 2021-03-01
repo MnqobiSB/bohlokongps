0cfa08c5230369b5d3d66ff2e5c1ee16c817c3bd
MnqobiSB <amandlamm1@gmail.com>
2021-03-01 13:50:31 +0200

    app updated after pc reset, latest news styles finished

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,52 @@ const router = express.Router();
 const { landingPage } = require('../controllers');
 const { asyncErrorHandler } = require('../middleware');
 
-// Front-End Routes
+// Index Routes
 
 /* GET home/landing page. */
 router.get('/', asyncErrorHandler(landingPage));
 
 // GET /about
-router.get('/about', (req, res) => {
-	res.send('Welocome to the About Us Page');
+router.get('/about-us', (req, res) => {
+	res.render('about', {
+		title: 'About Us',
+		page: 'about'
+	});
 });
 // Get /staff
 router.get('/staff', (req, res) => {
-	res.send('Welocome to the Staff Page');
+	res.render('staff', {
+		title: 'Meet The Staff',
+		page: 'staff'
+	});
 });
 // GET /gallery
 router.get('/gallery', (req, res) => {
-	res.send('Welocome to the Gallery Page');
+	res.render('gallery', {
+		title: 'Our Gallery',
+		page: 'gallery'
+	});
 });
 // GET /calender
 router.get('/calenda', (req, res) => {
-	res.send('Welocome to the Calenda Page');
+	res.render('calenda', {
+		title: 'School Calenda',
+		page: 'calenda'
+	});
 });
 // GET /documents
 router.get('/documents', (req, res) => {
-	res.send('Welocome to the Documents Page');
+	res.render('documents', {
+		title: 'Available Documents',
+		page: 'documents'
+	});
+});
+// GET /contact
+router.get('/contact', (req, res) => {
+	res.render('contact', {
+		title: 'Contact Us',
+		page: 'contact'
+	});
 });
 
 module.exports = router;
