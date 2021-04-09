0f21c1749dd6d6767f9143f7efe47ceede5dc29b
MnqobiSB <amandlamm1@gmail.com>
2021-04-09 13:12:59 +0200

    setup posts functionality and fix all bugs

--- models/user.js
+++ models/user.js
@@ -6,6 +6,10 @@ const UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
 	email: { type: String, unique: true, required: true },
+	isAdmin: {
+		type: Boolean,
+		default: false
+	},
 	image: {
 		secure_url: { type: String, default: '/images/default-profile.png' },
 		public_id: String
